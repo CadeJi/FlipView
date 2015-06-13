1da0877697e119358a5690e86aacf5cb14a93ca2
gruan01 <gruan@asnum.com>
2015-06-14 03:12:10 +0800

    WP Siliverlight FlipView based on ItemsControl and ScrollViewer

---
+++ FlipView/FlipView.WinPhone/Controls/FlipView.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace FlipView.WinPhone.Controls {
+
+    [TemplatePart(Name = SV, Type = typeof(ScrollViewer))]
+    [TemplatePart(Name = PT, Type = typeof(ItemsControl))]
+    public class FlipView : ItemsControl {
+
+        private const string SV = "SV";
+        private const string PT = "PT";
+
+        public ObservableCollection<bool> Points {
+            get;
+            set;
+        }
+
+        private int Count {
+            get {
+                return this.Items.Count;
+            }
+        }
+
+        private int currIdx = 0;
+        public int CurrIdx {
+            get {
+                return this.currIdx;
+            }
+            set {
+                this.currIdx = value < 0 ? 0 : (value >= this.Count ? this.Count - 1 : value);
+            }
+        }
+
+        #region dependency
+        public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register("Orientation", typeof(Orientation), typeof(FlipView), new PropertyMetadata(Orientation.Horizontal, Changed));
+
+        public Orientation Orientation {
+            get {
+                return (Orientation)this.GetValue(OrientationProperty);
+            }
+            set {
+                this.SetValue(OrientationProperty, value);
+            }
+        }
+        #endregion
+
+
+        private static void Changed(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+
+        }
+
+        public FlipView() {
+            this.DefaultStyleKey = typeof(FlipView);
+            this.Points = new ObservableCollection<bool>();
+        }
+
+        protected override void OnItemsChanged(System.Collections.Specialized.NotifyCollectionChangedEventArgs e) {
+            base.OnItemsChanged(e);
+            if (this.ItemsSource != null)
+                this.Points = new ObservableCollection<bool>(this.ItemsSource.Cast<object>().Select((o, i) => i == this.CurrIdx));
+        }
+
+        public override void OnApplyTemplate() {
+            base.OnApplyTemplate();
+
+            var sv = (ScrollViewer)this.GetTemplateChild(SV);
+            //sv.ManipulationCompleted += sv_ManipulationCompleted;
+            sv.AddHandler(ManipulationCompletedEvent, new EventHandler<ManipulationCompletedEventArgs>(sv_ManipulationCompleted), true);
+
+            var pt = (ItemsControl)this.GetTemplateChild(PT);
+            pt.ItemsSource = this.Points;
+        }
+
+        void sv_ManipulationCompleted(object sender, System.Windows.Input.ManipulationCompletedEventArgs e) {
+            if (e.TotalManipulation.Translation.X == 0)
+                return;
+
+            var sc = sender as ScrollViewer;
+            Debug.WriteLine("{0},{1}", sc.ExtentWidth, sc.HorizontalOffset);
+
+
+            this.Points[this.CurrIdx] = false;
+            this.CurrIdx += e.TotalManipulation.Translation.X > 0 ? -1 : 1;
+            this.Points[this.CurrIdx] = true;
+
+            var t = this.Width * this.CurrIdx;
+            //sc.ScrollToHorizontalOffset(t);
+            sc.ScrollToHorizontalOffset(this.CurrIdx);
+            e.Handled = true;
+        }
+
+        #region
+        protected override bool IsItemItsOwnContainerOverride(object item) {
+            return false;
+        }
+
+        protected override DependencyObject GetContainerForItemOverride() {
+            return new ContentControl() {
+                Width = this.Width,
+                Height = this.Height
+            };
+        }
+
+        //protected override void PrepareContainerForItemOverride(DependencyObject element, object item) {
+        //    var ct = (ContentControl)element;
+        //    ct.ContentTemplate = this.ItemTemplate;
+        //}
+        #endregion
+    }
+}
--- FlipView/FlipView.WinPhone/MainPage.xaml.cs
+++ FlipView/FlipView.WinPhone/MainPage.xaml.cs
@@ -7,15 +7,22 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Collections.ObjectModel;
 
 namespace FlipView.WinPhone {
     public partial class MainPage : global::Xamarin.Forms.Platform.WinPhone.FormsApplicationPage {
+
+        public ObservableCollection<string> Imgs {
+            get;
+            set;
+        }
+
         public MainPage() {
             InitializeComponent();
             SupportedOrientations = SupportedPageOrientation.PortraitOrLandscape;
 
-            global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new FlipView.App());
+            //global::Xamarin.Forms.Forms.Init();
+            //LoadApplication(new FlipView.App());
 
 
             //var fv = new Controls.FlipView() {
@@ -28,6 +35,22 @@ namespace FlipView.WinPhone {
             //};
 
             //this.Content = fv;
+
+            this.Imgs = new ObservableCollection<string>(){
+                "http://f.hiphotos.baidu.com/image/w%3D400/sign=cf3db92c0b24ab18e016e03705fbe69a/f703738da97739122e40d96bfa198618367ae252.jpg",
+                "http://e.hiphotos.baidu.com/image/w%3D400/sign=8b347e407c3e6709be0044ff0bc69fb8/e7cd7b899e510fb32396f5f0da33c895d0430ccd.jpg",
+                "http://e.hiphotos.baidu.com/image/w%3D400/sign=9915223123a446237ecaa462a8237246/11385343fbf2b2116724dd05c98065380cd78e77.jpg"
+            };
+
+            var fv = (FlipView.WinPhone.Controls.FlipView)this.FindName("fv");
+            fv.Items.Add(new Button() {
+                Content = "a"
+            });
+            fv.Items.Add(new TextBlock() {
+                Text = "b"
+            });
+
+            this.DataContext = this;
         }
     }
 }
