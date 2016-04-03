47219bf2f42c0d5ba506b02542f8dd5866310817
xlingfairy <gruan@asnum.com>
2016-04-04 00:21:11 +0800

    Auto replay for android

--- FlipView/FlipView.Droid/Renders/FlipViewAdapter.cs
+++ FlipView/FlipView.Droid/Renders/FlipViewAdapter.cs
@@ -10,9 +10,10 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.Support.V4.View;
+using Java.Lang;
 
 namespace FlipView.Droid.Renders {
-    public class FlipViewAdapter : PagerAdapter {
+    public class FlipViewAdapter : PagerAdapter, ViewPager.IOnPageChangeListener {
 
         private List<View> Items;
 
@@ -22,20 +23,24 @@ namespace FlipView.Droid.Renders {
             }
         }
 
-        private Context Ctx = null;
+        private ViewPager ViewPager = null;
 
-        public FlipViewAdapter(Context ctx, List<View> items) {
+        public FlipViewAdapter(ViewPager vp, List<View> items) {
             if (items == null)
                 throw new ArgumentNullException("items");
             if (items.Count() == 0)
                 throw new ArgumentException("items is empty", "items");
 
             this.Items = items;
-            this.Ctx = ctx;
+            this.ViewPager = vp;
         }
 
         public override bool IsViewFromObject(View view, Java.Lang.Object objectValue) {
             return view.Equals(objectValue);
+        }
+
+        public override int GetItemPosition(Java.Lang.Object objectValue) {
+            return PositionNone;
         }
 
         public override Java.Lang.Object InstantiateItem(ViewGroup container, int position) {
@@ -61,17 +66,64 @@ namespace FlipView.Droid.Renders {
             //container.RemoveView((View)chd);
         }
 
-        public override void FinishUpdate(ViewGroup container) {
-            var vp = container as ViewPager;
-            var pos = vp.CurrentItem;
 
-            if (pos == 0) {
-                pos = this.Items.Count;/////////////
-            } else if (pos == this.Count - 1) {
-                pos = this.Items.Count - 1;///////////////
-            }
-            System.Diagnostics.Debug.WriteLine("====> pos {0}", pos);
-            vp.SetCurrentItem(pos, false);
+        private bool IsManual = false;
+        /// <summary>
+        /// only occured by gesture
+        /// </summary>
+        /// <param name="state"></param>
+        public void OnPageScrollStateChanged(int state) {
+            this.IsManual = true;
+            //switch (state) {
+            //    case 1://手势滑动
+            //        this.isAutoPlay = false;
+            //        break;
+            //    case 2://界面切换
+            //        this.isAutoPlay = true;
+            //        break;
+            //    case 0://滑动结束
+            //        break;
+            //}
         }
+
+        public void OnPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            //Do nothing
+        }
+
+        public void OnPageSelected(int pos) {
+            if (IsManual) {
+                if (pos == 0) {
+                    pos = this.Items.Count;/////////////
+                    this.ViewPager.SetCurrentItem(pos, false);
+                }
+                else if (pos == this.Count - 1) {
+                    pos = this.Items.Count - 1;///////////////
+                    this.ViewPager.SetCurrentItem(pos, false);
+                }
+            }
+        }
+
+        public void Next() {
+            this.IsManual = false;
+            var pos = (this.ViewPager.CurrentItem + 1) % this.Items.Count;
+            this.ViewPager.SetCurrentItem(pos, false);
+        }
+
+        //public override void FinishUpdate(ViewGroup container) {
+        //    var vp = container as ViewPager;
+        //    var pos = vp.CurrentItem;
+
+        //    if (pos == 0) {
+        //        pos = this.Items.Count;/////////////
+        //        vp.SetCurrentItem(pos, false);
+        //    }
+        //    else if (pos == this.Count - 1) {
+        //        pos = this.Items.Count - 1;///////////////
+        //        vp.SetCurrentItem(pos, false);
+        //    }
+
+        //}
+
+
     }
 }
